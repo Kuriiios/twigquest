94e32df08a5485d342836845f15726ded2edbae3
Cyril <cyril.leconte07@gmail.com>
2024-05-14 16:00:59 +0200

    aie

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
 
 // Get a $twig object from this file.
+require_once '../vendor/autoload.php';
 require_once __DIR__ . '/../config/twig.php';
 
-
-$name = 'Wilder';
-
-echo $twig->render('home.html.twig', ['name' => $name]);
+require_once __DIR__ . '/../public/products.php';
